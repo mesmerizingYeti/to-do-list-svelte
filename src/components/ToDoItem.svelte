a0079bc9a21d1e1a24bd0b1bcfe0d13579fc8774
Nathanael Hauser <nathanaelhauser@gmail.com>
2021-08-16 18:03:29 -0700

    Added basic styling to ToDoItem component

---
+++ src/components/ToDoItem.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let title = "";
+</script>
+
+<div class="container">
+  <div class="task">
+    <input type="checkbox" name="to-do-checkbox" />
+    <label for="to-do-checkbox">{title}</label>
+  </div>
+  <button class="del-btn">Delete</button>
+</div>
+
+<style>
+  input {
+    margin: 0 0.5em 0 0;
+    padding: 0;
+  }
+  button {
+    margin: 0;
+  }
+  .container {
+    margin: 1em;
+    padding: 0.75em;
+    border: 1px solid black;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .task {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+  }
+</style>
